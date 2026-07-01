84a8950319a48d8a23d176b5d522bc9ef862eb3e
Putra Danialzulqarnain <putradanialzul765@gmail.com>
2026-07-01 09:44:38 +0800

    Added metadata to layout.tsx.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
-export default function Layout({children}){
-    return (<html>
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Pro-Portal",
+  description: "A centralized company management platform",
+  authors: [ { name: "Syntax Terror" } ]
+};
+
+
+export default function Layout({children, }: {children: React.ReactNode}){
+    return (
+    <html>
         <body>{children}</body>
-    </html>);
+    </html>
+    );
 }
